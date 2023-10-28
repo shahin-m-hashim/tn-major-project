254f1234f158d610a099ac13fa225bb993264d69
Shahin M Hashim <shahinmhashim@gmail.com>
2023-10-28 11:39:50 +0530

    installed material ui and added important folders to src

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
     return (
         <div className="App">
+            <Button variant="contained">Hello world</Button>
         </div>
     );
 }
