350a610b23a5b51d9306f1a3f3cd7f925d71424d
Shahin M Hashim <shahinmhashim@gmail.com>
2023-10-29 00:52:38 +0530

    completed services section using material ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Header from './components/header/Header.js';
 import HeroSection from './components/hero/HeroSection';
 import AboutSection from './components/about/About';
+import ServicesSection from './components/services/ServiceSection.js';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <main>
                 <HeroSection />
                 <AboutSection />
+                <ServicesSection />
             </main>
         </div>
     );
