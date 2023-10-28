d6d6e7d330c067c5d44f8cf346d66b91596b35c8
Shahin M Hashim <shahinmhashim@gmail.com>
2023-10-28 20:24:56 +0530

    completed home section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import Header from './components/header/Header.js';
+import HeroSection from './components/hero/HeroSection';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <main>
+                <HeroSection />
+            </main>
         </div>
     );
 }
---
+++ src/components/hero/HeroSection.js
@@ -0,0 +1,22 @@
+import './hero.css'
+import React from 'react';
+import Container from '@mui/material/Container';
+
+function HeroSection() {
+    return (
+        <div id="home" className="hero-section">
+            <section className="banner-one banner-img">
+                <Container maxWidth="xl">
+                    <div className="banner-text text-center">
+                        <h1>THE BEST <span>DIGITAL MARKETING</span> &amp; BRANDING</h1>
+                        <p>
+                            Welcome to Digital Boost Pro, where we specialize in the art of digital marketing and branding. Our passion lies in helping businesses thrive in the digital landscape, driving growth, and achieving success. With a team of creative minds and marketing experts, we're here to elevate your brand and create memorable experiences for your audience.
+                        </p>
+                    </div>
+                </Container>
+            </section>
+        </div>
+    );
+}
+
+export default HeroSection;
